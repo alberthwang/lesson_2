2d4610eba494706a53bfd6d85636cdbf34660930
Albert <10779212+alberthwang@users.noreply.github.com>
2024-04-10 02:35:22 -0700

    refactor display winner logic in own function

--- rockpaperscis.py
+++ rockpaperscis.py
@@ -17,6 +17,18 @@ import random
 
 VALID_CHOICES = ['rock', 'paper', 'scissors']
 
+def display_winner(choice, computer_choice):
+    if ((choice == "rock" and computer_choice == "scissors") or
+        (choice == "paper" and computer_choice == "rock") or
+        (choice == "scissors" and computer_choice == "paper")):
+        prompt("You win!")
+    elif ((choice == "rock" and computer_choice == "paper") or
+          (choice == "paper" and computer_choice == "scissors") or
+          (choice == "scissors" and computer_choice == "rock")):
+        prompt("Computer wins!")
+    else:
+        prompt("It's a tie!")
+
 def prompt(message):
   print(f'==> {message}')
   
@@ -31,16 +43,8 @@ while True:
     computer_choice = random.choice(VALID_CHOICES)
     prompt(f"You chose {choice}, computer chose {computer_choice}")
 
-    if ((choice == "rock" and computer_choice == "scissors") or
-        (choice == "paper" and computer_choice == "rock") or
-        (choice == "scissors" and computer_choice == "paper")):
-        prompt("You win!")
-    elif ((choice == "rock" and computer_choice == "paper") or
-          (choice == "paper" and computer_choice == "scissors") or
-          (choice == "scissors" and computer_choice == "rock")):
-        prompt("Computer wins!")
-    else:
-        prompt("It's a tie!")
+    display_winner(choice, computer_choice)
+    #is display winner returns a string, i can print that string instead.
 
     prompt("Do you want to play again (y/n)?")
     answer = input().lower()
