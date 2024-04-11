8036852bba51a2c4020da3375a5170af43c864d2
alberthwang <10779212+alberthwang@users.noreply.github.com>
2024-04-10 18:59:25 -0700

    refactor

--- calculator.py
+++ calculator.py
@@ -4,15 +4,39 @@
 # Perform the operation on the two numbers.
 # Print the result to the terminal.
 
-print("Welcome to calculator!")
-print("What's the first number?")
-num1 = input()
-print("what's the second number?")
-num2 = input()
+def prompt(say):
+    print(f'==> {say}')
 
-print("What is the operation you woulf like to perform? \n 1) Add 2) Subtract 3) Multiply 4) Divide")
+def invalid_number(num):
+    try:
+        int(num)
+    except(ValueError):
+        prompt('invalid input')
+        return True
+    
+    return False
+    
+prompt("Welcome to calculator!")
+prompt("What's the first number?")
+num1 = input()
+while invalid_number(num1):
+    prompt('Enter a number')
+    num1 = input()
+
+prompt("what's the second number?")
+num2 = input()
+while invalid_number(num2):
+    prompt('Enter a number')
+    num2 = input()
+    
+
+#prompt("What is the operation you woulf like to perform? \n 1) Add 2) Subtract 3) Multiply 4) Divide")
 
 operation = input()
+while operation not in ['1', '2', '3', '4']:
+    prompt('You must choose 1, 2, 3, or 4. \n1) Add 2) Subtract 3) Multiply 4) Divide')
+    operation = input()
+'''
 
 if operation == '1':
     output = int(num1) + int(num2)
@@ -22,5 +46,21 @@ elif operation == '3':
     output = int(num1) * int(num2)
 elif operation == '4':
     output = int(num1) / int(num2)
+
+'''
+output = 0
+match operation:
+    case '1':
+        output = int(num1) + int(num2)
+    case '2':
+        output = int(num1) - int(num2)
+    case '3':
+        output = int(num1) * int(num2)
+    case '4':
+        output = int(num1) / int(num2)
+
     
-print(f'The result is {output}')
+
+
+
+prompt(f'The result is {output}')
